734b50d39dd322570438125f972574ce5c0c2856
slavejko <s.slavejko@gmail.com>
2022-12-27 16:37:07 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function showQuetions(index){
 
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    let option_tag = '<div class="option"><select multiple><span>'+ questions[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
