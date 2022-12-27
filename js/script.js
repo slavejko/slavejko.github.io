0928f69e4569ad2907619889a84e2100a94746c5
slavejko <s.slavejko@gmail.com>
2022-12-27 15:37:19 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
+// const timeText = document.querySelector(".timer .time_left_txt");
+// const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
 start_btn.onclick = ()=>{
@@ -26,8 +26,8 @@ continue_btn.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
-    startTimer(15); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
+    // startTimer(15); //calling startTimer function
+    // startTimerLine(0); //calling startTimerLine function
 }
 
 let timeValue =  15;
@@ -54,8 +54,8 @@ restart_quiz.onclick = ()=>{
     queCounter(que_numb); //passing que_numb value to queCounter
     clearInterval(counter); //clear counter
     clearInterval(counterLine); //clear counterLine
-    startTimer(timeValue); //calling startTimer function
-    startTimerLine(widthValue); //calling startTimerLine function
+    // startTimer(timeValue); //calling startTimer function
+    // startTimerLine(widthValue); //calling startTimerLine function
     timeText.textContent = "Time Left"; //change the text of timeText to Time Left
     next_btn.classList.remove("show"); //hide the next button
 }
@@ -77,8 +77,8 @@ next_btn.onclick = ()=>{
         queCounter(que_numb); //passing que_numb value to queCounter
         clearInterval(counter); //clear counter
         clearInterval(counterLine); //clear counterLine
-        startTimer(timeValue); //calling startTimer function
-        startTimerLine(widthValue); //calling startTimerLine function
+        // startTimer(timeValue); //calling startTimer function
+        // startTimerLine(widthValue); //calling startTimerLine function
         timeText.textContent = "Time Left"; //change the timeText to Time Left
         next_btn.classList.remove("show"); //hide the next button
     }else{
@@ -165,45 +165,45 @@ function showResult(){
     }
 }
 
-function startTimer(time){
-    counter = setInterval(timer, 1000);
-    function timer(){
-        timeCount.textContent = time; //changing the value of timeCount with time value
-        time--; //decrement the time value
-        if(time < 9){ //if timer is less than 9
-            let addZero = timeCount.textContent;
-            timeCount.textContent = "0" + addZero; //add a 0 before time value
-        }
-        if(time < 0){ //if timer is less than 0
-            clearInterval(counter); //clear counter
-            timeText.textContent = "Time Off"; //change the time text to time off
-            const allOptions = option_list.children.length; //getting all option items
-            let correcAns = questions[que_count].answer; //getting correct answer from array
-            for(i=0; i < allOptions; i++){
-                if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                    console.log("Time Off: Auto selected correct answer.");
-                }
-            }
-            for(i=0; i < allOptions; i++){
-                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-            }
-            next_btn.classList.add("show"); //show the next button if user selected any option
-        }
-    }
-}
+// function startTimer(time){
+//     counter = setInterval(timer, 1000);
+//     function timer(){
+//         timeCount.textContent = time; //changing the value of timeCount with time value
+//         time--; //decrement the time value
+//         if(time < 9){ //if timer is less than 9
+//             let addZero = timeCount.textContent;
+//             timeCount.textContent = "0" + addZero; //add a 0 before time value
+//         }
+//         if(time < 0){ //if timer is less than 0
+//             clearInterval(counter); //clear counter
+//             timeText.textContent = "Time Off"; //change the time text to time off
+//             const allOptions = option_list.children.length; //getting all option items
+//             let correcAns = questions[que_count].answer; //getting correct answer from array
+//             for(i=0; i < allOptions; i++){
+//                 if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
+//                     option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+//                     option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+//                     console.log("Time Off: Auto selected correct answer.");
+//                 }
+//             }
+//             for(i=0; i < allOptions; i++){
+//                 option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+//             }
+//             next_btn.classList.add("show"); //show the next button if user selected any option
+//         }
+//     }
+// }
 
-function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
-    function timer(){
-        time += 1; //upgrading time value with 1
-        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
-            clearInterval(counterLine); //clear counterLine
-        }
-    }
-}
+// function startTimerLine(time){
+//     counterLine = setInterval(timer, 29);
+//     function timer(){
+//         time += 1; //upgrading time value with 1
+//         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+//         if(time > 549){ //if time value is greater than 549
+//             clearInterval(counterLine); //clear counterLine
+//         }
+//     }
+// }
 
 function queCounter(index){
     //creating a new span tag and passing the question number and total question
