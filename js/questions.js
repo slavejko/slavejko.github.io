67a0c19a1ccced4be8d4485f2f342a63da805ed7
slavejko <s.slavejko@gmail.com>
2022-12-28 14:19:15 +0100

    changes

--- js/questions.js
+++ js/questions.js
@@ -1,35 +1,86 @@
 let questions = [
-    {
-    numb: 1,
-    question: "Tioesterová väzba vznikne naviazaním:",
-    answer: ["acylu na síru -SH skupiny", "kyseliny fosforečnej na síru -SH skupiny"],
-    options: [
-      "acylu na kyselinu fosforečnú",
-      "acylu na síru -SH skupiny",
-      "kyseliny fosforečnej na síru -SH skupiny",
-      "kyseliny sírovej na -OH skupinu alkoholu"
-    ]
-  },
-    {
-    numb: 2,
-    question: "x",
-    answer: ["xx"],
-    options: [
-      "t",
-      "Colo",
-      "C",
-      "xx"
-    ]
-  },
   {
+    numb: 1,
+    question: " Tioesterová väzba vznikne naviazaním:",
+    answer: [" acylu na síru -SH skupiny"],
+    options: [" acylu na kyselinu fosforečnú"," acylu na síru -SH skupiny"," kyseliny fosforečnej na síru -SH skupiny"," kyseliny sírovej na -OH skupinu alkoholu"]
+ },
+ {
+    numb: 2,
+    question: " Ktorý z nasledujúcich nukleotidov je donorom energie pre proteosyntézu:",
+    answer: [" GTP: bielkoviny, glukogenéza","UTP: sacharidy","CTP: lipidy"],
+    options: [" ATP"," UTP"," GTP"," CTP"]
+ },
+ {
     numb: 3,
-    question: "Tioesterová väzba vznikne naviazaním:",
-    answer: ["acylu na síru -SH skupiny", "kyseliny fosforečnej na síru -SH skupiny"],
-    options: [
-      "acylu na kyselinu fosforečnú",
-      "acylu na síru -SH skupiny",
-      "kyseliny fosforečnej na síru -SH skupiny",
-      "kyseliny sírovej na -OH skupinu alkoholu"
-    ]
-  },
+    question: " Koľko makroergických väzieb je prítomných v molekule 1,3-bisfosfoglycerátu:",
+    answer: [" 1"],
+    options: [" 1"," 2"," 3"," žiadna makroergická väzba"]
+ },
+ {
+    numb: 4,
+    question: " Substrátom pre kreatínkinázu je:",
+    answer: [" kreatínfosfát + ADP"],
+    options: [" kreatínfosfát + ATP"," kreatínfosfát + ADP"," len kreatínfosfát"," kreatín + ADP"]
+ },
+ {
+    numb: 5,
+    question: " Substrátom pre kreatínkinázu je:",
+    answer: [" kreatínfosfát"," ADP"],
+    options: [" kreatinín"," kreatínfosfát"," AMP"," ADP"]
+ },
+ {
+    numb: 6,
+    question: " Keď donorom vodíkov pre dýchací reťazec je FADH2, potom:",
+    answer: [" koenzým Q je prvým akceptorom vodíkov, 2 molekuly ATP sa môžu vytvoriť pri oxidačnej fosforylácii","A nie lebo prvý akceptor vodíkov je ubichinón","C nie lebo sa prenesú 4 protony, lebo z 2 molekúl ubichinónu sa uvoľnia spolu 4 protóny"],
+    options: [" FMN je prvým akceptorom vodíkov"," koenzým Q je prvým akceptorom vodíkov"," 6 protónov je prenášaných do medzimembránového priestoru mitochondrie"," 2 molekuly ATP sa môžu vytvoriť pri oxidačnej fosforylácii"]
+ },
+ {
+    numb: 7,
+    question: " V hladkom endoplazmatickom retikule prebieha:",
+    answer: [" syntéza karboxylových kyselín, desaturácia karboxylových kyselín ... kniha str. 90, 97"],
+    options: [" syntéza karboxylových kyselín"," proteosyntéza"," glykolýza"," desaturácia karboxylových kyselín"]
+ },
+ {
+    numb: 8,
+    question: " H2O2:",
+    answer: [" vzniká pôsobením aeróbnych dehydrogenáz, katalázou je redukovaný na vodu, peroxidázou je redukovaný na vodu"," pôsobením anaeróbnych dehydrogenáz vzniká voda"," H2O je katalázou oxidovaná na O2 a redukovaná na vodu","H2O2 + H2O2 = 2 H2O + O2","Peroxidáza: AH2 + H2O2 = A + H2O"],
+    options: [" vzniká pôsobením aeróbnych dehydrogenáz"," katalázou je redukovaný na O2"," katalázou je redukovaný na vodu"," peroxidázou je redukovaný na vodu"]
+ },
+ {
+    numb: 9,
+    question: " Molekula FMN obsahuje:",
+    answer: [" ribitol + fosfát, riboflavín + fosfát  obasuje: flavin + ribitol + fosfat, resp. riboflavin + fosfat"],
+    options: [" ribitol + fosfát"," iba riboflavín"," riboflavín + fosfát"," riboflavín + adenozín + fosfát"]
+ },
+ {
+    numb: 10,
+    question: " Acylfosfátová makroergická väzba vzniká väzbou:",
+    answer: [" nič, napr. v kys. 1,3 bisfosfoglycerová"],
+    options: [" fosfátu na acetaldehyd"," fosfátu na -OH skupinu glyceraldehydu"," fosfátu na acyl-Coa"," acylu na glycerolfosfát"]
+ },
+ {
+    numb: 11,
+    question: " NADH2:",
+    answer: [" slúži ako donor vodíkov v dýchacom reťazci"," A nie, koenzým monooxygenáz je NADPH"," C nie, pri starej teórií 3 molekuly, novej 2,5"," D nie, lebo sa transportuje 6 protónov z matrix do medzimembránového, podľa novej teórie 10"],
+    options: [" je koenzýmom monooxygenáz"," slúži ako donor vodíkov v dýchacom reťazci"," v dýchacom reťazci umožňuje tvorbu 2 molekúl ATP"," pri jeho oxidácii v dýchacom reťazci sa transportuje 6 protónov do matrix mitochondrie"]
+ },
+ {
+    numb: 12,
+    question: " Kreatínkináza v bunkách kostrového svalu katalyzuje:",
+    answer: [" hydrolýzu kreatínfosfátu, regeneráciu ATP, tvorbu difosfátovej makroergickej väzby"],
+    options: [" hydrolýzu kreatínfosfátu"," regeneráciu ATP"," tvorbu difosfátovej makroergickej väzby"," tvorbu kreatinínu"]
+ },
+ {
+    numb: 13,
+    question: " V pokojovom stave je pomer ATP:ADP v svalovej bunke:",
+    answer: [" 10:1"],
+    options: [" 1:1"," 1:10"," 10:1"," 2:1"]
+ },
+ {
+    numb: 14,
+    question: " GTP je látka, ktorá:",
+    answer: [" obsahuje difosfátovú makroergickú väzbu, je donorom energie pri glukoneogenéze","C (je donorom energie pri glukoneogenéze) pri syntéze bielkovín","A (obsahuje guanidínfosfátovú makroergickú väzbu)  nie, guanínfosfátovú vazbu obsahuje kreatínfosfát"],
+    options: [" obsahuje guanidínfosfátovú makroergickú väzbu"," obsahuje difosfátovú makroergickú väzbu"," je donorom energie pri glukoneogenéze"," je priamym donorom energie pri svalovej práci"]
+ },
 ];
