0dca00be52776f7f6b6431462f87522f462abfc9
slavejko <s.slavejko@gmail.com>
2022-12-28 14:45:20 +0100

    changes

--- js/questions.js
+++ js/questions.js
@@ -83,4 +83,232 @@ let questions = [
     answer: [" obsahuje difosfátovú makroergickú väzbu"," je donorom energie pri glukoneogenéze"],
     options: [" obsahuje guanidínfosfátovú makroergickú väzbu", " obsahuje difosfátovú makroergickú väzbu", " je donorom energie pri glukoneogenéze", " je priamym donorom energie pri svalovej práci"]
   },
+  {
+    numb: 15,
+    question: " Enzýmy Krebsovho cyklu sú lokalizované:",
+    answer: [" v matrix mitochondrie"],
+    options: [" vo vonkajšej membráne mitochondrie"," v matrix mitochondrie"," v medzimembránovom priestore mitochondrie"," v cytozole"]
+ },
+ {
+    numb: 16,
+    question: " Účinkom laktátdehydrogenázy v smere oxidácie NADH2 sa vytvára:",
+    answer: [" laktát"],
+    options: [" pyruvát"," laktát"," citrát"," acetyl-Coa"]
+ },
+ {
+    numb: 17,
+    question: " Ktorý z týchto vitamínov môže byť zložkou v koenzýmoch oxidoreduktáz:",
+    answer: [" riboflavín, tiamín"],
+    options: [" pyridoxín"," riboflavín"," vitamín D"," tiamín"]
+ },
+ {
+    numb: 18,
+    question: " Guanozíndifosfát obsahuje väzbu:",
+    answer: [" Nglykozidovú"," fosfoesterovú"," fosfoanhydridovú"],
+    options: [" guanidínfosfátovú"," N-glykozidovú"," fosfoesterovú"," fosfoanhydridovú"]
+ },
+ {
+    numb: 19,
+    question: " Acylfosfátová makroergická väzba sa nachádza v:",
+    answer: [" 1,3bisfosfoglyceráte"],
+    options: [" 3-fosfoglyceráte"," 2,3-bisfosfoglyceráte"," 3-fosfoglyceraldehyde"," 1,3-bisfosfoglyceráte"]
+ },
+ {
+    numb: 20,
+    question: " Kreatínfosfokináza v pracujúcom svale katalyzuje:",
+    answer: [" tvorbu ATP"," tvorbu difosfátovej väzby"],
+    options: [" tvorbu ADP"," tvorbu ATP"," tvorbu kreatínfosfátu"," tvorbu difosfátovej väzby"]
+ },
+ {
+    numb: 21,
+    question: " Najvyššia aktivita kreatínfosfokinázy sa nachádza v:",
+    answer: ["  kostrovom svale"],
+    options: [" kostrovom svale"," myokarde"," pečeni"," mozgu"]
+ },
+ {
+    numb: 22,
+    question: " V molekule NAD+ sa nachádza heterocyklický kruh:",
+    answer: [" pyridínový"," purínový"],
+    options: [" pyridínový"," purínový"," tiazolový"," pyrolový"]
+ },
+ {
+    numb: 23,
+    question: " Cytochrómy:",
+    answer: [" obsahujú vo svojej molekule polypeptidový reťazec"," obsahujú ión železa viazaný hémovou skupinou"," sú schopné viazať elektróny"],
+    options: [" obsahujú vo svojej molekule polypeptidový reťazec"," obsahujú ión železa viazaný hémovou skupinou"," sú schopné viazať elektróny"," môžu viazať aj celý atóm vodíka"]
+ },
+ {
+    numb: 24,
+    question: " Redukciou butanolu vznikne:",
+    answer: [" nič"],
+    options: [" sekundárny alkohol"," 1-butanol"," primárny alkohol"," butanón"]
+ },
+ {
+    numb: 25,
+    question: " Monooxygenázy:",
+    answer: [" katalyzujú oxidoredukčné reakcie"," ich účinkom vznikajú hydroxyderiváty substrátov"],
+    options: [" katalyzujú oxidoredukčné reakcie"," sú dôležité pre tvorbu energie v bunke"," ich účinkom vznikajú hydroxyderiváty substrátov"," obsahujú ako koenzým NAD+"]
+ },
+ {
+    numb: 26,
+    question: " Makroergickou väzbou je väzba:",
+    answer: [" enolfosfátová"," tioesterová"," difosfátová"],
+    options: [" amidová"," enolfosfátová"," tioesterová"," difosfátová"]
+ },
+ {
+    numb: 27,
+    question: " Acylfosfátová väzba obsahuje:",
+    answer: [" nič"],
+    options: [" adenozíntrifosfát"," 3-fosfoglycerát"," fosfoenolpyruvát"," triacylglycerol"]
+ },
+ {
+    numb: 28,
+    question: " V pokojovom stave je v svalovej bunke vyššia koncentrácia:",
+    answer: [" kreatínfosfátu ako ATP"," kreatínfosfátu ako ADP"],
+    options: [" ADP ako ATP"," kreatínfosfátu ako ATP"," kreatínfosfátu ako ADP"," ATP ako kreatínfosfátu"]
+ },
+ {
+    numb: 29,
+    question: " FAD:",
+    answer: [" patrí medzi flavínové koenzýmy"," obsahuje vo svojej molekule vitamín B2"],
+    options: [" je koenzýmom hydroperoxidáz"," ak sa redukuje, priberá 2 protóny a 1 elektrón"," patrí medzi flavínové koenzýmy"," obsahuje vo svojej molekule vitamín B2"]
+ },
+ {
+    numb: 30,
+    question: " Vitamínovou zložkou NAD+ je:",
+    answer: [" nikotínamid, derivát pyridínu"],
+    options: [" tiamín"," nikotínamid"," riboflavín"," derivát pyridínu"]
+ },
+ {
+    numb: 31,
+    question: " Pri premene H2 na 2H+ dochádza ku:",
+    answer: [" oxidácii molekuly vodíka"],
+    options: [" oxidácii molekuly vodíka"," redukcii molekuly vodíka"," vzniku konjugovaného páru"," disociácii vodíka"]
+ },
+ {
+    numb: 32,
+    question: " Dehydrogenáciou kyseliny jantárovej vznikne kyselina:",
+    answer: [" fumárová"," buténdiová"],
+    options: [" jablčná"," fumárová"," buténdiová"," oxaloctová"]
+ },
+ {
+    numb: 33,
+    question: " V molekule 1,3-bisfosfoglycerátu sa nachádza:",
+    answer: [" acylfosfátová makroergická väzba"," fosfoesterová väzba"],
+    options: [" acylfosfátová makroergická väzba"," fosfoesterová makroergická väzba"," difosfátová makroergická väzba"," fosfoesterová väzba"]
+ },
+ {
+    numb: 34,
+    question: " Aký typ väzby vzniká medzi dvomi molekulami H3PO4:",
+    answer: [" difosfátová"],
+    options: [" acylfosfátová"," difosfátová"," fosfodiesterová"," enolfosfátová"]
+ },
+ {
+    numb: 35,
+    question: " Energia uložená v molekule ATP sa využíva na:",
+    answer: [" svalovú kontrakciu"," prenos nervového vzruchu"],
+    options: [" pasívny transport látok cez membrány buniek"," hydrolytickú degradáciu bielkovín"," svalovú kontrakciu"," prenos nervového vzruchu"]
+ },
+ {
+    numb: 36,
+    question: " Pri redukcii FAD na FADH2 sa na koenzým viaže:",
+    answer: [" 2 celé vodíky"," 2 protóny a 2 elektróny"],
+    options: [" 1 vodík a 1 protón"," 1 protón a 2 elektróny"," 2 celé vodíky"," 2 protóny a 2 elektróny"]
+ },
+ {
+    numb: 37,
+    question: " Redukciou butanónu vznikne:",
+    answer: [" sekundárny alkohol"," butanol?"],
+    options: [" butanal"," sekundárny alkohol"," butanol"," primárny alkohol"]
+ },
+ {
+    numb: 38,
+    question: " Amid kyseliny nikotínovej obsahuje heterocyklický kruh:",
+    answer: [" pyridínový"],
+    options: [" pyrimidínový"," imidazolový"," pyridínový"," purínový"]
+ },
+ {
+    numb: 39,
+    question: " Pri účinku katalázy na dve molekuly peroxidu vodíka:",
+    answer: [" oxidáciou sa vytvára molekula kyslíka"," redukciou vzniká voda"," prebieha dismutácia"],
+    options: [" oxidáciou sa vytvára molekula kyslíka"," redukciou vzniká voda"," 2 protóny sa redukujú na molekulu vodíka"," prebieha dismutácia"]
+ },
+ {
+    numb: 40,
+    question: " Aký typ väzby vzniká väzbou fosfátu na ribózu v adenozíne:",
+    answer: [" fosfoesterová"],
+    options: [" fosfoanhydridová"," acylfosfátová"," fosfoesterová"," enolfosfátová"]
+ },
+ {
+    numb: 41,
+    question: " Priamym donorom energie pre svalovú prácu je:",
+    answer: [" ATP"],
+    options: [" ATP"," laktát"," kreatínfosfát"," glukóza"]
+ },
+ {
+    numb: 42,
+    question: " Acylfosfátová makroergická väzba je prítomná v:",
+    answer: [" nič"],
+    options: [" ATP"," 3-fosfoglyceráte"," fosfoenolpyruváte"," kreatínfosfáte"]
+ },
+ {
+    numb: 43,
+    question: " GTP je látka, ktorá:",
+    answer: [" je donorom energie pri proteosyntéze"," obsahuje dve makroergické väzby"," obsahuje fosfoesterovú väzbu"],
+    options: [" je donorom energie pri proteosyntéze"," vzniká v kostrovom svale účinkom kreatínkinázy"," obsahuje dve makroergické väzby"," obsahuje fosfoesterovú väzbu"]
+ },
+ {
+    numb: 44,
+    question: " Substrátom laktátdehydrogenázy je:",
+    answer: [" laktát + NAD+"," pyruvát + NADH2"],
+    options: [" pyruvát + NAD+"," laktát + NADH2"," laktát + NAD+"," pyruvát + NADH2"]
+ },
+ {
+    numb: 45,
+    question: " Chinón pri premene na hydrochinón:",
+    answer: [" priberá 2 protóny a 2 elektróny"],
+    options: [" stráca 2 elektróny"," priberá 2 protóny a 2 elektróny"," priberá iba 2 protóny"," priberá iba 2 elektróny"]
+ },
+ {
+    numb: 46,
+    question: " Ktoré z uvedených transportérov prenášajú iba elektróny:",
+    answer: [" FeSproteín"," cytochróm b"],
+    options: [" FMN"," FeS-proteín"," koenzým Q"," cytochróm b"]
+ },
+ {
+    numb: 47,
+    question: " Dehydrogenázy sú enzýmy, ktoré:",
+    answer: [" môžu katalyzovať vratné reakcie"," sú súčasťou terminálnej oxidácie"],
+    options: [" využívajú ako koenzým hém"," nachádzajú sa len v cytosole bunky"," môžu katalyzovať vratné reakcie"," sú súčasťou terminálnej oxidácie"]
+ },
+ {
+    numb: 48,
+    question: " Fosfoanhydridová väzba vzniká väzbou fosfátu na:",
+    answer: ["  adenozínmonofosfát"," adenozíndifosfát"],
+    options: [" adenín"," adenozín"," adenozínmonofosfát"," adenozíndifosfát"]
+ },
+ {
+    numb: 49,
+    question: " Guanidínfosfátová makroergická väzba je prítomná v:",
+    answer: [" kreatínfosfáte"," arginínfosfáte"],
+    options: [" guanozíne"," GTP"," kreatínfosfáte"," arginínfosfáte"]
+ },
+ {
+    numb: 50,
+    question: " ATP obsahuje:",
+    answer: [" 2 makroergické väzby"],
+    options: [" 1 makroergickú väzbu"," 2 makroergické väzby"," 3 makroergické väzby"," žiadnu makroergickú väzbu"]
+ },
+ {
+    numb: 51,
+    question: " Aktivita kreatínkinázy je najvyššia v:",
+    answer: [" kostrovom svale"],
+    options: [" kostrovom svale"," srdci"," mozgu"," je rovnaká v uvedených tkanivách"]
+ },
+ {
+    numb: 52,
+    question: " V procese terminálnej oxidácie:",
+    answer: [" protóny sú transportované do medzimembránového priestoru"],
+    options: [" elektróny sú transportované do mitochondrie"," protóny sú transportované do medzimembránového priestoru"," 3 ATP sa vytvárajú z FADH2"," 2 ATP sa vytvárajú z NADH2"]
+ },
 ];
